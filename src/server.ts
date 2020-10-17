214965fbf24223456047be9a14163ff306822d67
DausterBarbosa <dausterbarbosa12@gmail.com>
2020-10-17 20:37:37 -0300

    cors refatorado

--- src/server.ts
+++ src/server.ts
@@ -14,10 +14,10 @@ import ErrorHandler from "./errors/handler";
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 app.use(routes);
 app.use("/uploads", express.static(path.join(__dirname, "..", "uploads")));
-app.use(cors());
 app.use(ErrorHandler);
 
 app.listen(3333);
